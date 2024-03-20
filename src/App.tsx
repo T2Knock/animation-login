17c093ba5136ad7645bcdbdd58956acb1706180e
knock-huynh <huynhtransongngoc@gmail.com>
2024-03-20 09:04:45 +0700

    chore(tailwind): config typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 	return (
-		<div className="m-4 flex h-screen w-screen">
-			<div className="h-full w-full bg-red-500"></div>
-			<div className="h-full w-full bg-blue-500"></div>
+		<div className="flex h-screen w-screen overflow-hidden">
+			<div className="h-full w-full"></div>
+			<div className="h-full w-full bg-gradient-to-r from-sky-500 to-teal-800"></div>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from 'tailwindcss-animate'
+
+export default {
 	darkMode: ['class'],
 	content: [
 		'./pages/**/*.{ts,tsx}',
@@ -15,6 +17,23 @@ module.exports = {
 			screens: {
 				'2xl': '1400px',
 			},
+		},
+		fontSize: {
+			sm: '0.750rem',
+			base: '1rem',
+			xl: '1.333rem',
+			'2xl': '1.777rem',
+			'3xl': '2.369rem',
+			'4xl': '3.158rem',
+			'5xl': '4.210rem',
+		},
+		fontFamily: {
+			heading: 'Poppins',
+			body: 'Poppins',
+		},
+		fontWeight: {
+			normal: '400',
+			bold: '700',
 		},
 		extend: {
 			colors: {
@@ -73,5 +92,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [tailwindcssAnimate],
 }
